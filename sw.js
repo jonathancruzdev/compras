f3fe412fb14f42f47a3f645b04542de452a32962
Jonathan CruzÂ <jonathan.cruz@davinci.edu.ar>
2022-11-17 15:16:35 -0300

    app

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
+const CACHE_NAME = 'cache-1';
 self.addEventListener('install', function(e){
     console.log(e);
+    const cache = caches.open(CACHE_NAME).then( cache => {
+        return cache.addAll([
+            'app.js',
+            'index.html',
+            'compras.png',
+            'style.css'
+        ])
+    })
+
+    e.waitUntil( cache );
+})
+
+self.addEventListener( 'fetch', e => {
+    const respuesCache = caches.match(e.request).then( res => {
+        if( res) {
+            res;
+        }
+        else {
+            return fetch( e.request)
+        }
+    })
+
+    e.respondWith(respuesCache)
 })

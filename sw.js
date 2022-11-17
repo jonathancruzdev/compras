4e0ccceb073c36481250d33522b3ad7ff11ca1f2
Jonathan CruzÂ <jonathan.cruz@davinci.edu.ar>
2022-11-17 15:43:04 -0300

    Correción de errores

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-1';
+const CACHE_NAME = 'cache-3' ;
 self.addEventListener('install', function(e){
     console.log(e);
     const cache = caches.open(CACHE_NAME).then( cache => {
@@ -14,14 +14,21 @@ self.addEventListener('install', function(e){
 })
 
 self.addEventListener( 'fetch', e => {
-    const respuesCache = caches.match(e.request).then( res => {
+    const respuestaCache = caches.match(e.request).then( res => {
         if( res) {
-            res;
-        }
-        else {
-            return fetch( e.request)
+            return res;
+        } else { // Si no está en el cache, busca en la web
+            return fetch( e.request).then( respuesta => {
+                // Ya que lo tengo de la web, lo guardo en el cache
+
+                caches.open( CACHE_NAME).then( cache => {
+                    cache.put( e.request, respuesta)
+                })
+
+                return respuesta.clone()
+            })
         }
     })
 
-    e.respondWith(respuesCache)
+    e.respondWith(respuestaCache)
 })
